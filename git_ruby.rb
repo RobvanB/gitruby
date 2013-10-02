9928874cbe6a70de63c6e99ecf38ce1a8b0495c9
Rob vanBrandenburg <RobvanB@gmail.com>
2013-10-01 21:16:42 -0400

    Started adding functionality where it reads the customer name from file and creates a subdir per customer.

--- git_ruby.rb
+++ git_ruby.rb
@@ -4,17 +4,18 @@ class Git_Ruby
   def initialize
     @xpodir    = '/xpotmp'
     @username  = 'RobvanB'
-    @password  =  ''
+    @password  =  'DRiZdIIDr23G'
     @cred      = { :login => @username, :password => @password }
+    @main_repo = "AKARepo" #Single repo, subdir per customer
   end
 
   def listFiles
     # Pull the emails with the XPO attachments, extract the attachments and move them to our input folder
-    if !system('./getEmailAttach.sh')
-      puts "Problem pulling in email"
-      exit
-    end
-
+    #if !system('./getEmailAttach.sh')
+    #  puts "Problem pulling in email"
+    # exit
+    #end
+    #TODO: Re-enable
     Dir.chdir(@xpodir)
     if Dir.pwd == @xpodir
       xpolist = Dir.glob("*.xpo")
@@ -24,30 +25,46 @@ class Git_Ruby
   end
 
   def parseFilename(filename)
-    filename.gsub('SharedProject_', "").gsub('.xpo', "")
+    filename = filename.gsub('.xpo', "") #Remove .xpo
+
+    #Grab the customer name from the commit msg (2nd line - for now, XML later ?)
+    line_counter = 1
+    file = File.new(filename + ".commit", "r")
+    while (line = file.gets)
+      if line_counter == 2
+        @customer = line.chomp  #Chomp removes new line character
+      end
+      line_counter = line_counter + 1
+    end
+    file.close
   end
 
   def find_or_create_repo(xpo_filename)
-    repo_name = self.parseFilename(xpo_filename)
+    #repo_name = self.parseFilename(xpo_filename)
+    self.parseFilename(xpo_filename)
     @client = Octokit::Client.new(@cred)
     # TODO: use Oauth
 
     xpo_filename_plus_path = @xpodir + "/" + xpo_filename
-    user_repo = { :username => @cred[:login], :repo => repo_name}
+    #user_repo = { :username => @cred[:login], :repo => repo_name}
+    user_repo = { :username => @cred[:login], :repo => @main_repo}
 
     begin
       # Check if we have a repo - if not, create it
       if !@client.repository?(user_repo)
-        puts "No Repo #{repo_name}, creating new repo"
-        @client.create_repository(repo_name , :auto_init => true)
+        #puts "No Repo #{repo_name}, creating new repo"
+        puts "No Repo #{@main_repo}, creating new repo"
+        #@client.create_repository(repo_name , :auto_init => true)
+        @client.create_repository(@main_repo , :auto_init => true)
         @repo_exists = false
         sleep 3
       else
-        puts "repo #{repo_name} exists."
+        #puts "repo #{repo_name} exists."
+        puts "repo #{@main_repo} exists."
         @repo_exists = true
       end
-    rescue
-      puts "Problem connecting to GitHub"
+    rescue => ex
+      puts "Problem connecting to GitHub: #{ex.message}"
       return
     end
 
@@ -55,6 +72,8 @@ class Git_Ruby
     puts "Repo: #{@repo}"
 
     sleep 2 #leave some time between the GIT API requests    (not sure if required)
+
+    puts "Customer : #{@customer}"
 
     # Use the API to add the new file and commit to the GitHb DB. This way we don't need a local git
     # See: http://developer.github.com/v3/git/
@@ -86,20 +105,39 @@ class Git_Ruby
         # Make sure we have the file
         # TODO: at this point we assume that we only have 1 xpo in the repo - this need to be reviewed/expanded
         tree_obj = @client.tree(user_repo, tree_sha)
-        puts tree_obj.inspect
+        cur_file = @customer + "/" + xpo_filename
+
+        #puts tree_obj.inspect
         tree_obj.tree.each{|t|
-          if t.path == xpo_filename
+          if t.path == cur_file
             @file_in_repo = true
             #puts t.sha
             @update_file_sha = t.sha
             puts "file found"
           else
             @file_in_repo = false
-            puts "#{xpo_filename} not found in repo #{repo_name}, adding..."
-            puts "INFO: #{user_repo} #{xpo_filename} #{xpo_filename_plus_path}"
-            content_and_commit = @client.create_contents(user_repo, xpo_filename, "Adding content", xpo_filename_plus_path )
+            puts "#{cur_file} not found in repo #{@main_repo}, adding..."
+            #puts "INFO: #{user_repo} #{xpo_filename} #{xpo_filename_plus_path}"
+            content_and_commit = @client.create_contents(user_repo, cur_file, "Adding content", xpo_filename_plus_path )
           end
+
+
+          #if t.path == xpo_filename
+          #  @file_in_repo = true
+          #  #puts t.sha
+          #  @update_file_sha = t.sha
+          #  puts "file found"
+          #else
+          #  @file_in_repo = false
+          #  puts "#{xpo_filename} not found in repo #{@main_repo}, adding..."
+          #  puts "INFO: #{user_repo} #{xpo_filename} #{xpo_filename_plus_path}"
+          #  content_and_commit = @client.create_contents(user_repo, xpo_filename, "Adding content", xpo_filename_plus_path )
+          #end
         }
+
+        ####
+        exit
+        ####
 
         if @file_in_repo == true
           puts "updating...."
